873cf77df3c552a52e3efd6d0c32ca69aaca7a16
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-09-21 08:33:07 +0800

    实现循环队列

---
+++ javaImp/Main_LoopQueue.java
@@ -0,0 +1,19 @@
+package javaImp;
+
+import javaImp.Queue.LoopQueue;
+
+public class Main_LoopQueue {
+    public static void main(String[] args) {
+        LoopQueue<Integer> queue = new LoopQueue<>();
+
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
