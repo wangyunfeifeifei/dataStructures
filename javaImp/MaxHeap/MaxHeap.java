e8cdce54ba84d8e8d3addb6f5fce1ab31681b125
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-10-21 10:58:54 +0800

    增加最大堆的基础结构

---
+++ javaImp/MaxHeap/MaxHeap.java
@@ -0,0 +1,67 @@
+package javaImp.MaxHeap;
+
+import javaImp.Array.Array;
+
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    /**
+     * 返回堆中有多少元素
+     *
+     * @return
+     */
+    public int size() {
+        return data.getSize();
+    }
+
+    /**
+     * 返回堆是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    /**
+     * 返回父亲节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int parent(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("index - 0 doesn't have parent");
+        }
+        return (index - 1) / 2;
+    }
+
+    /**
+     * 返回左孩子的索引
+     *
+     * @param index
+     * @return
+     */
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    /**
+     * 返回右孩子的索引
+     *
+     * @param index
+     * @return
+     */
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+}
