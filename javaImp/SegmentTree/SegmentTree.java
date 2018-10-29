b30b03117e562cfa24273c3479e386248af1dea2
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-10-29 09:07:19 +0800

    增加线段树更新操作

--- javaImp/SegmentTree/SegmentTree.java
+++ javaImp/SegmentTree/SegmentTree.java
@@ -43,6 +43,7 @@ public class SegmentTree<E> {
 
     /**
      * 查询指定区间的内容
+     *
      * @param queryL
      * @param queryR
      * @return
@@ -57,11 +58,12 @@ public class SegmentTree<E> {
 
     /**
      * 查询指定区间内容具体实现
-     * @param treeIndex   树的节点索引
-     * @param l 该节点表示的区间下界
-     * @param r 该节点表示的区间的上界
-     * @param queryL 需要查询的区间下界
-     * @param queryR 需要查询的区间的上界
+     *
+     * @param treeIndex 树的节点索引
+     * @param l         该节点表示的区间下界
+     * @param r         该节点表示的区间的上界
+     * @param queryL    需要查询的区间下界
+     * @param queryR    需要查询的区间的上界
      * @return
      */
     private E query(int treeIndex, int l, int r, int queryL, int queryR) {
@@ -83,6 +85,49 @@ public class SegmentTree<E> {
         E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
         E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
         return merger.merge(leftResult, rightResult);
+    }
+
+    /**
+     * 将index位置的值更新
+     *
+     * @param index
+     * @param e
+     */
+    public void set(int index, E e) {
+        if (index < 0 || index >= data.length) {
+            throw new IllegalArgumentException("Index is illegal");
+        }
+
+        data[index] = e;
+
+        set(0, 0, data.length - 1, index, e);
+    }
+
+    /**
+     * 更新以treeIndex为根的线段树
+     *
+     * @param treeIndex
+     * @param l
+     * @param r
+     * @param index
+     * @param e
+     */
+    private void set(int treeIndex, int l, int r, int index, E e) {
+        if (l == r) {
+            tree[treeIndex] = e;
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        if (index >= mid + 1) {
+            set(rightTreeIndex, mid + 1, r, index, e);
+        } else {
+            set(leftTreeIndex, l, mid, index, e);
+        }
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
     }
 
     /**
